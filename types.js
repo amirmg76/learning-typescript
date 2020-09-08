3ccaad63382399085ab187d9da14290ff847a28d
amir ghazanfari <amir_ghazanfari@outlook.com>
2020-09-09 00:02:31 +0430

    try for project map

---
+++ types.js
@@ -0,0 +1,8 @@
+var json = '{"x": 10, "y": 20}';
+var coordiantes = JSON.parse(json);
+console.log(coordiantes);
+var new_ar = [];
+new_ar.push(2);
+new_ar.push(new Date());
+new_ar.push('hello');
+console.log(new_ar);
---
+++ types.ts
@@ -0,0 +1,9 @@
+const jso = '{"x": 10, "y": 20}';
+const coordiante = JSON.parse(jso);
+console.log(coordiantes);
+
+const new_arr: any[] = [];
+new_arr.push(2);
+new_arr.push(new Date());
+new_arr.push('hello');
+console.log(new_arr);
